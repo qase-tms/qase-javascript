1bb9ce8d6bba0c47980d4aa27420527645848c6a
Parviz Khavari <csctclan@gmail.com>
2020-06-04 20:30:50 +0300

    Use async/await in projects tests

--- test/services/projects.test.ts
+++ test/services/projects.test.ts
@@ -13,24 +13,22 @@ describe('Project api', () => {
         {offset: 30},
         {limit: 10},
     ]).forEach((params) => {
-        it('Get all projects', () => {
+        it('Get all projects', async () => {
             const content = list(project())
             mock.onGet("/project").reply(200, statusTrue(content))
             const client = new QaseApi('123')
-            client.projects.getAll(params.limit, params.offset).then((resp: AxiosResponse<ProjectList>) => {
-                expect(resp.config.params).toEqual(params)
-                expect(resp.data).toEqual(content as ProjectList)
-            })
+            const resp: AxiosResponse<ProjectList> = await client.projects.getAll(params.limit, params.offset)
+            expect(resp.config.params).toEqual(params)
+            expect(resp.data).toEqual(content as ProjectList)
         })
     })
 
-    it('Get specific project', () => {
+    it('Get specific project', async() => {
         const content = project()
         mock.onGet("/project/TEST").reply(200, statusTrue(content))
         const client = new QaseApi('123')
-        client.projects.get("TEST").then((resp: AxiosResponse<ProjectInfo>) => {
-            expect(resp.data).toEqual(content as ProjectInfo)
-        })
+        const resp: AxiosResponse<ProjectInfo> = await client.projects.get("TEST")
+        expect(resp.data).toEqual(content as ProjectInfo)
     })
 
 
@@ -38,23 +36,21 @@ describe('Project api', () => {
         {status: 200, content: project(), equal: true},
         {status: 404, content: {}, equal: false},
     ]).forEach(({status, content, equal}) => {
-        it('Check project exists', () => {
+        it('Check project exists', async () => {
             mock.onGet("/project/TEST").reply(status, statusTrue(content))
             const client = new QaseApi('123')
-            client.projects.exists("TEST").then((exists: boolean) => {
-                expect(exists).toEqual(equal)
-            })
+            const exists: boolean = await client.projects.exists("TEST")
+            expect(exists).toEqual(equal)
         })
     })
 
-    it('Create new project', () => {
+    it('Create new project', async () => {
         const content = {code: "TEST"}
         mock.onPost("/project").reply(200, statusTrue(content))
         const client = new QaseApi('123')
         const create = new ProjectCreate("new project", "TEST")
-        client.projects.create(create).then((resp: AxiosResponse<ProjectCreated>) => {
-            expect(resp.config.data).toEqual(JSON.stringify(create))
-            expect(resp.data).toEqual(content as ProjectCreated)
-        })
+        const resp: AxiosResponse<ProjectCreated> = await client.projects.create(create)
+        expect(resp.config.data).toEqual(JSON.stringify(create))
+        expect(resp.data).toEqual(content as ProjectCreated)
     })
 })
