ac841774df9cb7e6b3824558d05675870cfa08cd
Parviz Khavari <csctclan@gmail.com>
2020-07-25 18:56:03 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Typescript Template
+
+Set `NPM_TOKEN` secret variable on Github to use full power of Github Actions and publish your project on NPM.
---
+++ test/plugin.test.ts
@@ -0,0 +1,8 @@
+import 'jest';
+import TyprescriptTemplate from '../src';
+
+describe('Tests', () => {
+    it('Init main class', () => {
+        new TyprescriptTemplate();
+    });
+});
