0d32d2121fc2f8517e5430bdb6c1718bb446a0e1
Parviz Khavari <csctclan@gmail.com>
2021-02-07 22:24:13 +0300

    Add demo project

---
+++ demo/cypress/integration/sample_spec.js
@@ -0,0 +1,26 @@
+import { qase } from 'cypress-qase-reporter/dist/mocha';
+
+describe('My First Test', () => {
+    qase(1, it('clicking "type" navigates to a new url', () => {
+        cy.visit('https://example.cypress.io');
+
+        cy.contains('type').click();
+
+        // Should be on a new URL which includes '/commands/actions'
+        cy.url().should('include', '/commands/actions');
+    }));
+
+    qase([2, 3], it('Gets, types and asserts', () => {
+        cy.visit('https://example.cypress.io');
+
+        cy.contains('type').click();
+
+        // Should be on a new URL which includes '/commands/actions'
+        cy.url().should('include', '/commands/actions');
+
+        // Get an input, type into it and verify that the value has been updated
+        cy.get('.action-email')
+            .type('fake@email.com')
+            .should('have.value', 'fake@email.com');
+    }));
+});
