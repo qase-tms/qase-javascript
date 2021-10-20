b44920c2f9928a576e8bcb15baa7c7fab8fe6cb7
Evgeny Kochetkov <evgeny.kochetkov@me.com>
2021-10-20 16:02:33 +0300

    [jest] use time_ms instead of time when sending results

--- qase-jest/src/index.ts
+++ qase-jest/src/index.ts
@@ -252,7 +252,8 @@ class QaseReporter implements Reporter {
                                 caseId,
                                 Statuses[test.status],
                                 {
-                                    time: test.duration ? Math.ceil(test.duration / 1000) : undefined,
+                                    // eslint-disable-next-line camelcase, @typescript-eslint/no-non-null-assertion
+                                    time_ms: test.duration!,
                                     stacktrace: test.failureMessages.join('\n'),
                                     comment: test.failureMessages.length > 0 ? test.failureMessages.map(
                                         (value) => value.split('\n')[0]
