517a6bbf5e700247e5f7bcece4a7d2c56e563405
AKhabarow <akhabarow@gmail.com>
2021-12-02 19:55:16 +0300

    Migrating to the OpenApi-generator-cli

--- qaseio/src/qaseio.ts
+++ qaseio/src/qaseio.ts
@@ -1,54 +1,60 @@
 import axios, { AxiosInstance, AxiosRequestConfig } from 'axios';
-import { Attachments } from './services/attachments';
-import { Cases } from './services/cases';
-import { CustomFields } from './services/customFields';
-import { Defects } from './services/defects';
-import { Milestones } from './services/milestones';
-import { Plans } from './services/plans';
-import { Projects } from './services/projects';
-import { Results } from './services/results';
-import { Runs } from './services/runs';
-import { SharedSteps } from './services/sharedSteps';
-import { Suites } from './services/suites';
-import { Users } from './services/users';
-
+import { AttachmentsApi } from './api/attachments-api';
+import { CasesApi } from './api/cases-api';
+import { Configuration } from '.';
+import { CustomFieldsApi } from './api/custom-fields-api';
+import { DefectsApi } from './api/defects-api';
+import { MilestonesApi } from './api/milestones-api';
+import { PlansApi } from './api/plans-api';
+import { ProjectsApi } from './api/projects-api';
+import { ResultsApi } from './api/results-api';
+import { RunsApi } from './api/runs-api';
+import { SharedStepsApi } from './api/shared-steps-api';
+import { SuitesApi } from './api/suites-api';
+import { UsersApi } from './api/users-api';
 
 export class QaseApi {
-    public projects: Projects;
-    public cases: Cases;
-    public results: Results;
-    public runs: Runs;
-    public attachments: Attachments;
-    public plans: Plans;
-    public suites: Suites;
-    public milestones: Milestones;
-    public sharedSteps: SharedSteps;
-    public defects: Defects;
-    public customFields: CustomFields;
-    public users: Users;
+    public projects: ProjectsApi;
+    public cases: CasesApi;
+    public results: ResultsApi;
+    public runs: RunsApi;
+    public attachments: AttachmentsApi;
+    public plans: PlansApi;
+    public suites: SuitesApi;
+    public milestones: MilestonesApi;
+    public sharedSteps: SharedStepsApi;
+    public defects: DefectsApi;
+    public customFields: CustomFieldsApi;
+    public users: UsersApi;
 
     private api: AxiosInstance;
 
-    public constructor(apiToken: string) {
+    public constructor(apiToken: string, basePath?: string) {
         const config: AxiosRequestConfig = {
             headers: {
                 Token: apiToken,
             },
-            baseURL: 'https://api.qase.io/v1',
+            baseURL: basePath || 'http://api.qase.io/v1',
         };
+
+        const configuration = new Configuration({ apiKey: apiToken, basePath, baseOptions: {
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        } });
         this.api = axios.create(config);
 
-        this.projects = new Projects(this.api);
-        this.cases = new Cases(this.api);
-        this.results = new Results(this.api);
-        this.runs = new Runs(this.api);
-        this.attachments = new Attachments(this.api);
-        this.plans = new Plans(this.api);
-        this.suites = new Suites(this.api);
-        this.milestones = new Milestones(this.api);
-        this.sharedSteps = new SharedSteps(this.api);
-        this.defects = new Defects(this.api);
-        this.customFields = new CustomFields(this.api);
-        this.users = new Users(this.api);
+        this.projects = new ProjectsApi(configuration, basePath, this.api);
+        this.cases = new CasesApi(configuration, basePath, this.api);
+        this.results = new ResultsApi(configuration, basePath, this.api);
+        this.runs = new RunsApi(configuration, basePath, this.api);
+        this.attachments = new AttachmentsApi(configuration, basePath, this.api);
+        this.plans = new PlansApi(configuration, basePath, this.api);
+        this.suites = new SuitesApi(configuration, basePath, this.api);
+        this.milestones = new MilestonesApi(configuration, basePath, this.api);
+        this.sharedSteps = new SharedStepsApi(configuration, basePath, this.api);
+        this.defects = new DefectsApi(configuration, basePath, this.api);
+        this.customFields = new CustomFieldsApi(configuration, basePath, this.api);
+        this.users = new UsersApi(configuration, basePath, this.api);
     }
 }
