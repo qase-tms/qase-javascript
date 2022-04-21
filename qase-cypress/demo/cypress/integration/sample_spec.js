bd00ac6682c55ca599d6769e0fb283da34cdc4bb
AKhabarow <akhabarow@gmail.com>
2022-04-21 12:40:48 +0400

    Added failed testcase

--- qase-cypress/demo/cypress/integration/sample_spec.js
+++ qase-cypress/demo/cypress/integration/sample_spec.js
@@ -34,6 +34,6 @@ describe('My First Test', () => {
         // Get an input, type into it and verify that the value has been updated
         cy.get('.action-email')
             .type('fake@email.com')
-            .should('have.value', 'fake@email.com');
+            .should('have.value', 'fak@email.com');
     }));
 });
