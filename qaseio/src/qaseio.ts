2b6ed2f597a0b8d306c27383b19803c178d1c06a
iiojib <iiojib.vi@gmail.com>
2023-09-04 20:25:27 +0300

    Fix qaseio eslint issue

--- qaseio/src/qaseio.ts
+++ qaseio/src/qaseio.ts
@@ -86,6 +86,7 @@ export class QaseApi implements QaseApiInterface {
     axiosRetry(transport, {
       retries,
       retryDelay: () => retryDelay,
+      // eslint-disable-next-line @typescript-eslint/unbound-method
       retryCondition: axiosRetry.isIdempotentRequestError,
     });
 
