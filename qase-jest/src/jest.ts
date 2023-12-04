c311620fa25a303ce7bde6cb30c8a923277478ad
Tyler Santos <tylersantos@zoneandco.com>
2023-12-04 10:06:39 -0500

    Issue 473: Updating newName to only include parameters if parameters are passed in

--- qase-jest/src/jest.ts
+++ qase-jest/src/jest.ts
@@ -1,17 +1,17 @@
 /* eslint-disable no-console */
 export = {
-    qase: (caseId: number | string | number[] | string[], name: string, params?: object): string => {
+    qase: (caseId: number | string | number[] | string[], name: string, parameters?: object): string => {
         let caseIds: number[] | string[];
         if (typeof caseId === 'string' || typeof caseId === 'number') {
             caseIds = [caseId.toString()];
         } else {
             caseIds = caseId;
         }
-        let parameters = {};
-        if (typeof params === 'object') {
-            parameters = params;
+        let parameterString = ``;
+        if (typeof parameters === 'object') {
+            parameterString = ` [Parameters: ${JSON.stringify(parameters)}]`;
         }
-        const newName = `${name} (Qase ID: ${caseIds.join(',')}) [Parameters: ${JSON.stringify(parameters)}]`
+        const newName = `${name} (Qase ID: ${caseIds.join(',')}) ${parameterString}`
         return newName;
     },
 };
