1eb08274038af567fd1f556a0c8836a7ddb75b5e
Dmitrii Gridnev <dgridnev@qase.io>
2024-05-29 16:26:49 +0200

    qase-testcafe: add a new syntax
    - add new syntax to annotate the following fields: `QaseID`, `QaseTitle`, `QaseFields`, `QaseParameters`
    - add support suites

---
+++ qase-testcafe/src/qase.ts
@@ -0,0 +1,100 @@
+// eslint-disable-next-line @typescript-eslint/no-extraneous-class
+export class qase {
+  private static _qaseID = '';
+  private static _qaseTitle = '';
+  private static _qaseFields = '';
+  private static _qaseParameters = '';
+
+  /**
+   * Set a Qase ID for the test case
+   * Don't forget to call `create` method after setting all the necessary parameters
+   * @param {number | number[]} value
+   * @example
+   * const q = qase.id(1).create();
+   * test.meta(q)('Test case title', async t => { ... });
+   * or
+   * test.meta({userField: 123, ...q})('Test case title', async t => { ... });
+   */
+  public static id = (value: number | number[]) => {
+    this._qaseID = Array.isArray(value) ? value.join(',') : String(value);
+    return this;
+  };
+
+  /**
+   * Set a title for the test case
+   * Don't forget to call `create` method after setting all the necessary parameters
+   * @param {string} value
+   * @example
+   * const q = qase.title('Test case title').create();
+   * test.meta(q)('Test case title', async t => { ... });
+   * or
+   * test.meta({userField: 123, ...q})('Test case title', async t => { ... });
+   */
+  public static title = (value: string) => {
+    this._qaseTitle = value;
+    return this;
+  };
+
+  /**
+   * Set a fields for the test case
+   * Don't forget to call `create` method after setting all the necessary parameters
+   * @param {Record<string, string>} values
+   * @example
+   * const q = qase.fields({ 'severity': 'high', 'priority': 'medium' }).create();
+   * test.meta(q)('Test case title', async t => { ... });
+   * or
+   * test.meta({userField: 123, ...q})('Test case title', async t => { ... });
+   */
+  public static fields = (values: Record<string, string>) => {
+    this._qaseFields = this.toNormalizeRecord(values);
+    return this;
+  };
+
+  /**
+   * Set a parameters for the test case
+   * Don't forget to call `create` method after setting all the necessary parameters
+   * @param {Record<string, string>} values
+   * @example
+   * const q = qase.parameters({ 'severity': 'high', 'priority': 'medium' }).create();
+   * test.meta(q)('Test case title', async t => { ... });
+   * or
+   * test.meta({userField: 123, ...q})('Test case title', async t => { ... });
+   */
+  public static parameters = (values: Record<string, string>) => {
+    this._qaseParameters = this.toNormalizeRecord(values);
+    return this;
+  };
+
+  /**
+   * Create a Qase metadata
+   * Call this method after setting all the necessary parameters
+   * @example
+   * const q = qase.id(1).title('Test case title').fields({ 'severity': 'high', 'priority': 'medium' }).create();
+   * test.meta(q)('Test case title', async t => { ... });
+   * or
+   * test.meta({userField: 123, ...q})('Test case title', async t => { ... });
+   */
+  public static create = () => {
+    const meta = {
+      QaseID: this._qaseID,
+      QaseTitle: this._qaseTitle,
+      QaseFields: this._qaseFields,
+      QaseParameters: this._qaseParameters,
+    };
+
+    this._qaseID = '';
+    this._qaseTitle = '';
+    this._qaseFields = '';
+    this._qaseParameters = '';
+
+    return meta;
+  };
+
+  private static toNormalizeRecord = (record: Record<string, string>) => {
+    const stringRecord: Record<string, string> = {};
+    for (const [key, value] of Object.entries(record)) {
+      stringRecord[String(key)] = String(value);
+    }
+    return JSON.stringify(stringRecord);
+  };
+}
