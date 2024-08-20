053429ede53952720ce00e46ecba0d147dc41ae5
Dmitrii Gridnev <dgridnev@qase.io>
2024-08-20 10:32:41 +0200

    refactor: models in qase-javascript-commons
    Migrate some models from the interface to the class

--- qase-javascript-commons/src/models/index.ts
+++ qase-javascript-commons/src/models/index.ts
@@ -1,6 +1,6 @@
-export { type TestResultType, Relation, Suite, SuiteData } from './test-result';
+export { TestResultType, Relation, Suite, SuiteData } from './test-result';
 export { TestExecution, TestStatusEnum } from './test-execution';
-export { type TestStepType, StepType } from './test-step';
+export { TestStepType, StepType } from './test-step';
 export { StepStatusEnum } from './step-execution';
 export { Attachment } from './attachment';
 export { Report } from './report';
--- qase-javascript-commons/src/models/step-execution.ts
+++ qase-javascript-commons/src/models/step-execution.ts
@@ -5,9 +5,16 @@ export enum StepStatusEnum {
   skipped = 'skipped',
 }
 
-export interface StepExecution {
+export class StepExecution {
   start_time: number | null;
   status: StepStatusEnum;
   end_time: number | null;
   duration: number | null;
+
+  constructor() {
+    this.status = StepStatusEnum.passed;
+    this.start_time = null;
+    this.end_time = null;
+    this.duration = null;
+  }
 }
