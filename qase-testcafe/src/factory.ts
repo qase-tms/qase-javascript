1617bd55c20c77e79e47bad9d7dc2405c968f9b7
Dmitrii Gridnev <dgridnev@qase.io>
2024-08-29 16:47:25 +0200

    feature: improve error collection
    The error stack trace contains more useful debugging information: browser, code, etc.

--- qase-testcafe/src/factory.ts
+++ qase-testcafe/src/factory.ts
@@ -15,8 +15,20 @@ export const factory = (options: TestcafeQaseOptionsType) => {
     reportFixtureStart: () => {
       /* empty */
     },
-    reportTestDone: async (name: string, testRunInfo: TestRunInfoType, meta: Record<string, string>) => {
-      await reporter.reportTestDone(name, testRunInfo, meta);
+    async reportTestDone(
+      name: string,
+      testRunInfo: TestRunInfoType,
+      meta: Record<string, string>
+    ): Promise<void> {
+      return reporter.reportTestDone(
+        name,
+        testRunInfo,
+        meta,
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-expect-error Inject testrail error formatting method with bound context
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-member-access,@typescript-eslint/no-unsafe-argument
+        this.formatError.bind(this)
+      );
     },
     reportTaskDone: async () => {
       await reporter.reportTaskDone();
