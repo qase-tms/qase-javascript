a8ac393abffbe666001de7596800e2f638689773
Dmitrii Gridnev <dgridnev@qase.io>
2024-09-16 16:17:37 +0200

    feature: the ability to specify a test metadata in tests
    - `qase.title` - set the test title
    - `qase.fields` - set the test fields
    - `qase.suite` - set the test suite
    - `qase.comment` - set the test comment
    - `qase.parameters` - set the test parameters
    - `qase.groupParameters` - set the test group parameters
    - `qase.ignore` - ignore the test in Qase
    
    ```ts
    it('test', () => {
      qase.title('Title');
      qase.fields({ field: 'value' });
      qase.suite('Suite');
      qase.comment('Comment');
      qase.parameters({ param: 'value' });
      qase.groupParameters({ group: { param: 'value' } });
      qase.ignore();
    
      cy.visit('https://example.com');
    });

---
+++ qase-cypress/src/metadata.js
@@ -0,0 +1,52 @@
+import { MetadataManager } from './metadata/manager';
+
+module.exports = function(on) {
+  on('task', {
+    qaseTitle(value) {
+      MetadataManager.setTitle(value);
+      return null;
+    },
+  });
+
+  on('task', {
+    qaseFields(value) {
+      MetadataManager.setFields(value);
+      return null;
+    },
+  });
+
+  on('task', {
+    qaseIgnore() {
+      MetadataManager.setIgnore();
+      return null;
+    },
+  });
+
+  on('task', {
+    qaseParameters(value) {
+      MetadataManager.setParameters(value);
+      return null;
+    },
+  });
+
+  on('task', {
+    qaseGroupParameters(value) {
+      MetadataManager.setGroupParams(value);
+      return null;
+    },
+  });
+
+  on('task', {
+    qaseSuite(value) {
+      MetadataManager.setSuite(value);
+      return null;
+    },
+  });
+
+  on('task', {
+    qaseComment(value) {
+      MetadataManager.setComment(value);
+      return null;
+    },
+  });
+};
