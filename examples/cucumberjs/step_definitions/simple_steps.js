b0de270702393600c5dd51718ae9e1761e483a11
Dmitrii Gridnev <dgridnev@qase.io>
2024-11-11 14:56:08 +0100

    feat: support attachments in the test results
    ```js
    const { Given, Then } = require('cucumber');
    
    Given('I have a step with attachment', async function () {
      await this.attach('Hello, world!', 'text/plain');
    });
    
    Then('I have a step with attachment', async function () {
      await this.attach('Hello, world!', 'text/plain');
    });
    ```

--- examples/cucumberjs/step_definitions/simple_steps.js
+++ examples/cucumberjs/step_definitions/simple_steps.js
@@ -2,6 +2,7 @@ const { Given, When, Then } = require('@cucumber/cucumber');
 
 Given('I have a step', function() {
   console.log('I have a step');
+  this.attach('I\'m an attachment', 'text/plain');
 });
 
 Given('I have another step', function() {
