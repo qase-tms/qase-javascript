53500969bcd4045a2ad737d4090dcc5d36dcb782
Dmitrii Gridnev <dgridnev@qase.io>
2025-05-08 10:44:41 +0200

    fix: ensure run start time is before first test execution
    - Fixed issue where test run start time could be later than the first test start

--- qase-javascript-commons/src/client/dateUtils.ts
+++ qase-javascript-commons/src/client/dateUtils.ts
@@ -2,18 +2,17 @@
 const pad = (num: number) => num.toString().padStart(2, '0');
 
 export function formatUTCDate(date: Date): string {
-    const year = date.getUTCFullYear();
-    const month = pad(date.getUTCMonth() + 1);
-    const day = pad(date.getUTCDate());
-    const hours = pad(date.getUTCHours());
-    const minutes = pad(date.getUTCMinutes());
-    const seconds = pad(date.getUTCSeconds());
+  const year = date.getUTCFullYear();
+  const month = pad(date.getUTCMonth() + 1);
+  const day = pad(date.getUTCDate());
+  const hours = pad(date.getUTCHours());
+  const minutes = pad(date.getUTCMinutes());
+  const seconds = pad(date.getUTCSeconds());
 
-    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+  return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
 }
 
 export function getStartTime(): string {
-    const date = new Date();
-    date.setSeconds(-10);
-    return formatUTCDate(date);
+  const date = new Date();
+  return formatUTCDate(new Date(date.getTime() - 10000));
 }
