87c344d5a630a2ad215b76ab9f4f42057d2a3b0f
Dmitrii Gridnev <dgridnev@qase.io>
2026-02-16 15:09:26 +0300

    chore(07-01): prepare Jest examples for API testing scenarios
    - Add testTimeout: 10000 to jest.config.js for API request handling
    - Delete all old feature-demo test files (9 files)
    - Remove markdownContent.js helper
    - Remove attachments directory

--- examples/single/jest/test/ignore.test.js
+++
@@ -1,9 +0,0 @@
-const { qase } = require("jest-qase-reporter/jest");
-const {describe, test, expect} = require("@jest/globals");
-
-describe("Example: ignore.test.js", () => {
-  test("This test is executed using Jest; however, it is NOT reported to Qase", () => {
-    qase.ignore();
-    expect(true).toBe(true);
-  });
-});
