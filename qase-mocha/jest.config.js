9059f8024a22e768dfcab7b1ab5b358581259255
Dmitrii Gridnev <dgridnev@qase.io>
2026-04-29 11:28:00 +0300

    chore(mocha): add jest + ts-jest test infrastructure

---
+++ qase-mocha/jest.config.js
@@ -0,0 +1,16 @@
+/* eslint-disable */
+module.exports = {
+  preset: 'ts-jest',
+  roots: ['<rootDir>/test'],
+  transform: {
+    '^.+\\.ts$': ['ts-jest', {
+      tsconfig: {
+        skipLibCheck: true,
+      },
+    }],
+  },
+  testMatch: ['**/*.test.ts'],
+  moduleFileExtensions: ['js', 'ts'],
+  collectCoverage: false,
+  testEnvironment: 'node',
+};
